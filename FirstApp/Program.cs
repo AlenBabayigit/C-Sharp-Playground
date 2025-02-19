7f72bb1384e2728b00607ff585cd82657a5e9668
Alen Babayigit <alenbabayigit@gmail.com>
2025-02-19 05:39:18 +0300

    Create FirstApp
    Hello World!

---
+++ FirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment
+            /* 
+             * 
+             * This is a multiline comment
+             */
+            Console.WriteLine("Hello World!");  // This is my first line of C# code
+        }
+    }
+}
